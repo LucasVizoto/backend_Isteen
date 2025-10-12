abca59a038e27f0e51ae0bc9a729837194369824
Lucas Vizoto <lucasvizoto364@gmail.com>
2025-10-11 21:40:37 -0300

    refactor: moving tests to your own directories

--- src/controllers/game_deleter_controller.py
+++ src/controllers/game_deleter_controller.py
@@ -1,2 +1,10 @@
-class GameDeleterController:
-    pass
+from .interfaces.game_deleter_controller_interface import GameDeleterControllerInterface
+from src.models.sqlite.interfaces.games_repository_interface import GamesRepositoryInterface
+
+class GameDeleterController(GameDeleterControllerInterface):
+    
+    def __init__(self, game_repository: GamesRepositoryInterface) -> None:
+        self.__game_repository = game_repository
+
+    def delete_game(self, game_id: str) -> None:
+        self.__game_repository.delete_game(game_id)
